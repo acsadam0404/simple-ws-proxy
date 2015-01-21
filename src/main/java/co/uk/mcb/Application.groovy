835032ff9c6a056d2712c9add9a4989b3e31254b
Ács Ádám <acsadam0404@gmail.com>
2015-01-21 21:45:57 +0100

    refactor restapi location

--- src/main/java/co/uk/mcb/Application.groovy
+++ src/main/java/co/uk/mcb/Application.groovy
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 
-import co.uk.mcb.math.BigDecimalRounding
-
 @ComponentScan
 @EnableAutoConfiguration
 class Application {
